ff3cb5a046053fe5ec7dd3b5938424492da50062
Leonid Sliusar <loslyusar@gmail.com>
2023-06-06 23:47:12 +0300

    + makefile

--- banksite/banksite/settings.py
+++ banksite/banksite/settings.py
@@ -88,6 +88,16 @@ WSGI_APPLICATION = 'banksite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT')}
+#     }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
